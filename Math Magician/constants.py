ccc641bf36a000213e3b64afa594cb3c2fd5f082
zachjrt <77033009+zachjrt@users.noreply.github.com>
2022-05-02 18:18:41 -0700

    Add files via upload
    Basic upload

---
+++ Math Magician/constants.py
@@ -0,0 +1,14 @@
+import os
+
+
+import os
+import sys
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+
+ROOT = os.path.dirname(sys.modules['__main__'].__file__)
+BACKGROUND_IMAGE = ROOT + "/images/background.png" 
+MINOTAUR = ROOT + "/images/minotaur.png" 
+SLIME = ROOT + "/images/slime.png" 
+WIZARD= ROOT + "/images/wizard.png" 
---
+++ Math Magician/main.py
@@ -0,0 +1,82 @@
+import random
+import arcade
+from constants import *
+
+
+class Point:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+class Player():
+    def __init__(self):
+        super().__init__()
+        self.center = Point()
+        self.center.y = SCREEN_HEIGHT/2
+        self.center.x = SCREEN_WIDTH/4
+        self.angle = 0
+        self.image = WIZARD
+        self.texture = arcade.load_texture(self.image)
+    def draw(self):
+        arcade.draw_texture_rectangle(self.center.x, self.center.y, self.texture.width, self.texture.height, self.texture, self.angle, 255)
+        self.angle -= 2
+        self.center.x += 1
+    def advance(self):
+        self.center.y += 0
+        self.center.x += 0
+
+class Enemy():
+    def __init__(self):
+        super().__init__()
+        self.center = Point()
+        self.center.y = SCREEN_HEIGHT/2
+        self.center.x = SCREEN_WIDTH - 300
+        self.angle = 0
+        self.image = MINOTAUR
+        self.texture = arcade.load_texture(self.image)
+    def draw(self):
+        arcade.draw_texture_rectangle(self.center.x, self.center.y, self.texture.width, self.texture.height, self.texture, self.angle, 255)
+        self.angle += 2
+        self.center.x -= 1
+       
+    def advance(self):
+        self.center.y += 0
+        self.center.x += 0
+
+
+
+class Game(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.background = arcade.load_texture(BACKGROUND_IMAGE)
+        self.player = Player()
+        self.enemy = Enemy()
+        self.held_keys = set()
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.player.draw()
+        self.enemy.draw()
+    def update(self, delta_time):
+        self.check_keys()
+        #self.check_collisions()
+    def check_keys(self):
+        if arcade.key.LEFT in self.held_keys or arcade.key.A in self.held_keys:
+            print("left")
+            
+
+        if arcade.key.RIGHT in self.held_keys or arcade.key.D in self.held_keys:
+            print("left")
+
+        if arcade.key.UP in self.held_keys or arcade.key.W in self.held_keys:
+            print("left")
+
+        if arcade.key.DOWN in self.held_keys or arcade.key.S in self.held_keys:
+            print("left")
+
+
+    
+WINDOW = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+arcade.run()
